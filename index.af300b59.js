d35c1b6d1c50e65c916bb729762a341de7f86550
Victor Prisacariu <victor.prisacariu@evozon.com>
2023-11-07 13:20:34 +0200

    Updates

---
+++ index.af300b59.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-thanks]"),closeModalBtn:document.querySelector("[data-modal-close-thanks]"),modal:document.querySelector("[data-modal-thanks]")};function t(){e.modal.classList.toggle("is-hidden-thanks")}e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t)}();
+//# sourceMappingURL=index.af300b59.js.map
